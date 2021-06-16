557856847b0f93b0b768f678231dfdac717fabec
Hawty1 <49696027+mariusErpenbach@users.noreply.github.com>
2021-06-16 13:27:58 +0200

    added background-animations

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,14 @@ import React from "react";
 const Welcome = () => {
     return (
       <div class="page">
-        
+        <div class="background-balls">
+          <div></div>
+          <div></div> 
+        </div>
+        <div class="background-balls">
+          <div></div>
+          <div></div> 
+        </div>
       </div>
     );
   };
--- src/components/navBar.js
+++ src/components/navBar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavBar = () => {
     return (
-      <div class="page">
+      <div>
         
       </div>
     );

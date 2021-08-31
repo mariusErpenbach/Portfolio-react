d17a3c442ed671372fb0885f3a359c1c67349aae
Hawty1 <49696027+mariusErpenbach@users.noreply.github.com>
2021-08-31 08:18:25 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import Welcome from "./components/Welcome"
 import Blog from "./components/Blog"
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,6 +3,7 @@ const Welcome = () => {
     return (
       <div className="page" id="welcome-page">
         <p id="welcome-message"> My own little space</p>
+        <div className="welcome-article"></div>
       </div>
     );
   };

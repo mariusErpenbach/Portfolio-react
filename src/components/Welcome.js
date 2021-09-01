0ca4a5aee7e8fcedb56e9c073909c1d9395a18f5
Hawty1 <49696027+mariusErpenbach@users.noreply.github.com>
2021-09-01 23:41:27 +0200

    update

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,16 +1,24 @@
 import React from "react";
 const Welcome = () => {
+
+  const showPreview =() =>{
+    const element = document.querySelector('#myself-preview')
+  
+    
+    
+ 
+  }
     return (
       <div className="page" id="welcome-page">
         <p id="welcome-message"> My own little space</p>
         <div className="my-space"> 
+        <div className="planets" id="me-planet" onClick={showPreview}><div className="planet-line"></div><p>hi</p></div>
         <div className="planets"><div className="planet-line"></div></div>
         <div className="planets"><div className="planet-line"></div></div>
         <div className="planets"><div className="planet-line"></div></div>
         <div className="planets"><div className="planet-line"></div></div>
-        <div className="planets"><div className="planet-line"></div></div>
-      
         </div>
+        <div id="myself-preview"></div>
       </div>
       
     );

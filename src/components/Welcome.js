5af70c3ac6ef0f5d181900de973683202b1b92b4
Hawty1 <49696027+mariusErpenbach@users.noreply.github.com>
2021-09-02 23:56:13 +0200

    update

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,26 +1,23 @@
 import React from "react";
+
 const Welcome = () => {
 
-  const showPreview =() =>{
-    const element = document.querySelector('#myself-preview')
-  
-    
-    
- 
+  const handleClick= (e) =>{
+    console.log(e.target.id)
   }
     return (
       <div className="page" id="welcome-page">
         <p id="welcome-message"> My own little space</p>
         <div className="my-space"> 
-        <div className="planets" id="me-planet" onClick={showPreview}><div className="planet-line"></div><p>hi</p></div>
-        <div className="planets"><div className="planet-line"></div></div>
-        <div className="planets"><div className="planet-line"></div></div>
-        <div className="planets"><div className="planet-line"></div></div>
-        <div className="planets"><div className="planet-line"></div></div>
+        <div className="planets" id="me-planet" onClick={handleClick}>
+        <div className="planet-line"><div className="planet-line-dot"></div></div></div>
+        <div className="planets"><div className="planet-line"><div className="planet-line-dot"></div></div></div>
+        <div className="planets"><div className="planet-line"><div className="planet-line-dot"></div></div></div>
+        <div className="planets"><div className="planet-line"><div className="planet-line-dot"></div></div></div>
+        <div className="planets"><div className="planet-line"><div className="planet-line-dot"></div></div></div>
         </div>
         <div id="myself-preview"></div>
       </div>
-      
     );
   };
   
